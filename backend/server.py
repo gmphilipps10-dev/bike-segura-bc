ff5b57a7335c6d11cfb02e136815ec9abc4f95ff
gmphilipps10-dev <gmphilipps10@gmail.com>
2026-04-20 20:29:56 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -16,7 +16,7 @@ from jose import JWTError, jwt
 from bson import ObjectId
 
 ROOT_DIR = Path(__file__).parent
-load_dotenv(ROOT_DIR / '.env')
+load_dotenv(ROOT_DIR / '.env'override=False)
 
 # MongoDB connection with Atlas-friendly settings
 mongo_url = os.environ['MONGO_URL']
